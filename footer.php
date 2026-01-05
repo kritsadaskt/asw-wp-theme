8d99499f9c4a42173ea3b45749b8f8c267447c0a
kritsadaskt <kritsada.s@assetwise.co.th>
2026-01-05 13:21:06 +0700

    เพิ่มปุ่ม loan calc

--- footer.php
+++ footer.php
@@ -106,6 +106,12 @@ $f_footer = get_field('footer_code');
           <div class="pointer" onclick="show_sub()">
             <img id="footer_img_btn" src="<?= get_template_directory_uri() ?>/img/<?= pll_current_language() ?>/subscribe.png" class="inline-block pointer" width="100%">
           </div>
+          <a href="/<?=pll_current_language()?>/loan-calculator/" title="<?php pll_e('โปรแกรมคำนวนสินเชื่อ') ?>" class="flex items-center gap-2 border-[0.5px] border-[#a3aab0] rounded-[5px] px-7 py-1 w-[250px] mt-4">
+            <div class="calc-icon w-5 h-5">
+              <svg viewBox="0 0 24 24" fill="none" style="fill: none !important" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3.46447 20.5355C4.92893 22 7.28595 22 12 22C16.714 22 19.0711 22 20.5355 20.5355C22 19.0711 22 16.714 22 12C22 7.28595 22 4.92893 20.5355 3.46447C19.0711 2 16.714 2 12 2C7.28595 2 4.92893 2 3.46447 3.46447C2 4.92893 2 7.28595 2 12C2 16.714 2 19.0711 3.46447 20.5355Z" stroke="#ffffff" stroke-width="1.5"></path> <path d="M18 8.49998H14M18 14.5H14M18 17.5H14M10 8.49999H8M8 8.49999L6 8.49999M8 8.49999L8 6.49998M8 8.49999L8 10.5M9.5 14.5L8.00001 16M8.00001 16L6.50001 17.5M8.00001 16L6.5 14.5M8.00001 16L9.49999 17.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+            </div>
+            <span class="text-white"><?php pll_e('โปรแกรมคำนวนสินเชื่อ')?></span>
+          </a>
         </inner>
       </box>
       <sp class="l mobile-only"></sp>
